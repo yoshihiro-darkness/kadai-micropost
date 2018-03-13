ad9544343c247ccd334e758691f875dc85d61a91
yoshihiro-darkness <darkness.okkake@gmail.com>
2018-03-13 10:55:07 +0000

    follow, unfollow

--- app/User.php
+++ app/User.php
@@ -42,4 +42,56 @@ class User extends Model implements AuthenticatableContract,
 	{
 		return $this->hasMany(Micropost::class);
 	}
+	// add 2018.03.13
+	public function followings()
+	{
+		return $this->belongsToMany(User::class, 'user_follow', 'user_id', 'follow_id')->withTimestamps();
+	}
+	public function followers()
+	{
+		return $this->belongsToMany(User::class, 'user_follow', 'user_id', 'follow_id')->withTimestamps();
+	}
+	// add 2018.03.13
+	public function follow($userId) {
+	
+	$exist = $this->is_following($userId);
+
+	$its_me = $this->id == $userId;
+
+		if ($exist || $its_me) {
+
+			//既にフォローしていば何もしない
+
+			return false;
+
+		} else {
+
+			// 未フォローであればフォローする
+
+			$this->followings()->attach($userId);
+
+			return true;
+		}
+	}
+	public function unfollow($userId) {
+	
+	$exist = $this->is_following($userId);
+
+	$its_me = $this->id == $userId;
+
+		if ($exist && !$its_me) {
+
+			//既にフォローしていばフォローを外す
+			$this->followings()->detach($userId);
+			return true;
+		} else {
+
+			// 未フォローであれば何もしない
+			return false;
+		}
+	}
+
+	public function is_following($userId) {
+		return $this->followings()->where('follow_id',$userId)->exists();
+	}
 }
--- resources/views/users/followings.blade.php
+++ resources/views/users/followings.blade.php
@@ -11,17 +11,15 @@
                     <img class="media-object img-rounded img-responsive" src="{{ Gravatar::src($user->email, 500) }}" alt="">
                 </div>
             </div>
-				@include('user_follow.follow_button', ['user' => $user])
+            @include('user_follow.follow_button', ['user' => $user])
         </aside>
         <div class="col-xs-8">
             <ul class="nav nav-tabs nav-justified">
                 <li role="presentation" class="{{ Request::is('users/' . $user->id) ? 'active' : '' }}"><a href="{{ route('users.show', ['id' => $user->id]) }}">Microposts <span class="badge">{{ $count_microposts }}</span></a></li>
-                <li role="presentation" class="{{ Request::is('users/*/followings' . $user->id) ? 'active' : '' }}"><a href="{{ route('users.followings', ['id' => $user->id]) }}">Followings <span class="badge">{{ $count_followings }}</span></a></li>
-                <li role="presentation" class="{{ Request::is('users/*/followers' . $user->id) ? 'active' : '' }}"><a href="{{ route('users.followers', ['id' => $user->id]) }}">Followers <span class="badge">{{ $count_followers }}</span></a></li>
+                <li role="presentation" class="{{ Request::is('users/*/followings') ? 'active' : '' }}"><a href="{{ route('users.followings', ['id' => $user->id]) }}">Followings <span class="badge">{{ $count_followings }}</span></a></li>
+                <li role="presentation" class="{{ Request::is('users/*/followers') ? 'active' : '' }}"><a href="{{ route('users.followers', ['id' => $user->id]) }}">Followers <span class="badge">{{ $count_followers }}</span></a></li>
             </ul>
-            @if (count($microposts) > 0)
-                @include('microposts.microposts', ['microposts' => $microposts])
-            @endif
+            @include('users.users', ['users' => $users])
         </div>
     </div>
 @endsection
