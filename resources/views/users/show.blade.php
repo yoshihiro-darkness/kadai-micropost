1b0364a7181fde1369a89ce89b94a138eb3c0107
yoshihiro-darkness <darkness.okkake@gmail.com>
2018-03-17 08:07:51 +0000

    modify the place of nav

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -17,9 +17,9 @@
         <div class="col-xs-8">
             <ul class="nav nav-tabs nav-justified">
                 <li role="presentation" class="{{ Request::is('users/' . $user->id) ? 'active' : '' }}"><a href="{{ route('users.show', ['id' => $user->id]) }}">Microposts <span class="badge">{{ $count_microposts }}</span></a></li>
-                <li role="presentation" class="{{ Request::is('users/*/favoritings' . $user->id) ? 'active' : '' }}"><a href="{{ route('users.favoritings', ['id' => $user->id]) }}">Favoritings <span class="badge">{{ $count_favoritings }}</span></a></li>
                 <li role="presentation" class="{{ Request::is('users/*/followings' . $user->id) ? 'active' : '' }}"><a href="{{ route('users.followings', ['id' => $user->id]) }}">Followings <span class="badge">{{ $count_followings }}</span></a></li>
                 <li role="presentation" class="{{ Request::is('users/*/followers' . $user->id) ? 'active' : '' }}"><a href="{{ route('users.followers', ['id' => $user->id]) }}">Followers <span class="badge">{{ $count_followers }}</span></a></li>
+                <li role="presentation" class="{{ Request::is('users/*/favoritings' . $user->id) ? 'active' : '' }}"><a href="{{ route('users.favoritings', ['id' => $user->id]) }}">Favoritings <span class="badge">{{ $count_favoritings }}</span></a></li>
             </ul>
             @if (count($microposts) > 0)
                 @include('microposts.microposts', ['microposts' => $microposts])
